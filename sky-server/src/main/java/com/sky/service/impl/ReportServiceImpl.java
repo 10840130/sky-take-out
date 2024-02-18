7abf3764d9f1c12943f6ec77ed27d79f13415906
10840130 <k035884236@gmail.com>
2024-02-18 21:31:01 +0800

    用戶資料統計業務代碼開發

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.util.StringUtil;
@@ -25,15 +27,11 @@ public class ReportServiceImpl implements ReportService {
     @Autowired
     private OrderMapper orderMapper;
 
-    /**
-     * 統計指定時間內的營業額資料
-     *
-     * @param begin
-     * @param end
-     * @return
-     */
-    public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
-        //用於存放begin -> end範圍內的每天日期
+    @Autowired
+    private UserMapper userMapper;
+
+
+    private List<LocalDate> getDateList(LocalDate begin,LocalDate end){
         List<LocalDate> dateList = new ArrayList<>();
 
         dateList.add(begin);
@@ -43,6 +41,29 @@ public class ReportServiceImpl implements ReportService {
             begin = begin.plusDays(1);
             dateList.add(begin);
         }
+        return dateList;
+    }
+
+    /**
+     * 統計指定時間內的營業額資料
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
+        /*//用於存放begin -> end範圍內的每天日期
+        List<LocalDate> dateList = new ArrayList<>();
+
+        dateList.add(begin);
+
+        while (!begin.equals(end)) {
+            //計算指定日期的後一天對應日期
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }*/
+
+        List<LocalDate> dateList = getDateList(begin, end);
 
         List<Double> turnoverList = new ArrayList<>();
         for (LocalDate date : dateList) {
@@ -64,6 +85,52 @@ public class ReportServiceImpl implements ReportService {
                 .builder()
                 .dateList(StringUtils.join(dateList, ","))
                 .turnoverList(StringUtils.join(turnoverList, ","))
+                .build();
+    }
+
+    /**
+     * 用戶統計
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+
+        //存放日期
+        List<LocalDate> dateList = getDateList(begin, end);
+
+        //存放每天新增用戶數量 select count(id) from user where create_time < ? and create_time > ?
+        List<Integer> newUserList =new ArrayList<>();
+        //存放每天的總用戶數量 select count(id) from user where create_time < ?
+        List<Integer> totalUserList =new ArrayList<>();
+
+        for (LocalDate date : dateList) {
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            Map map =new HashMap<>();
+            map.put("end",endTime);
+            //總用戶數量
+            Integer totalUser = userMapper.countByMap(map);
+
+            map.put("begin",beginTime);
+            //新增用戶數量
+            Integer newUser = userMapper.countByMap(map);
+
+            totalUserList.add(totalUser);
+            newUserList.add(newUser);
+        }
+
+
+
+
+        //封裝VO資料
+        return UserReportVO
+                .builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .totalUserList(StringUtils.join(totalUserList, ","))
+                .newUserList(StringUtils.join(newUserList, ","))
                 .build();
     }
 }
